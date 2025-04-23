a346635b27dcec1669ce38cfb92b162ca9b8d8b2
Lenander88 <167141130+Lenander88@users.noreply.github.com>
2025-04-23 14:22:09 +0200

    rewriting

--- 23H2.ps1
+++ 23H2.ps1
@@ -17,82 +17,7 @@ if ($serialNumber) {
     wpeutil shutdown
 }
 
-###################Load Assembly for creating form & button######
-  
-[void][System.Reflection.Assembly]::LoadWithPartialName( "System.Windows.Forms")
-[void][System.Reflection.Assembly]::LoadWithPartialName( "Microsoft.VisualBasic")
-  
-  
-#####Define the form size & placement
-  
-$form = New-Object "System.Windows.Forms.Form";
-$form.Width = 500;
-$form.Height = 150;
-$form.Text = "Digital Workplace Group Tag";
-$form.StartPosition = [System.Windows.Forms.FormStartPosition]::CenterScreen;
-$form.ControlBox = $True
-  
-  
-##############Define text label2
-  
-$textLabel2 = New-Object "System.Windows.Forms.Label";
-$textLabel2.Left = 25;
-$textLabel2.Top = 45;
-$textLabel2.Text = "Group tag";
-  
-  
-############Define text box2 for input
-  
-$cBox2 = New-Object "System.Windows.Forms.combobox";
-$cBox2.Left = 150;
-$cBox2.Top = 45;
-$cBox2.width = 200;
-$cBox2.Text = "Choose group tag"
-  
-  
-###############"Add descriptions to combo box"##############
 
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Lenander88/L88/main/grouptags.csv' -Outfile grouptags.csv 
-Import-CSV ".\grouptags.csv" | ForEach-Object {
-    $cBox2.Items.Add($_.grouptags)| out-null
-      
-}
-  
-  
-#############define OK button
-$button = New-Object "System.Windows.Forms.Button";
-$button.Left = 360;
-$button.Top = 45;
-$button.Width = 100;
-$button.Text = "OK";
-$Button.Cursor = [System.Windows.Forms.Cursors]::Hand
-#$Button.Font = New-Object System.Drawing.Font("Times New Roman",10,[System.Drawing.FontStyle]::Regular)
-############# This is when you have to close the form after getting values
-$eventHandler = [System.EventHandler]{
-$cBox2.Text;
-$form.Close();};
-$button.Add_Click($eventHandler) ;
-  
-#############Add controls to all the above objects defined
-$form.Controls.Add($button);
-$form.Controls.Add($textLabel2);
-$form.Controls.Add($cBox2);
-#$ret = $form.ShowDialog();
-  
-#################return values
-$button.add_Click({
-      
-    #Set-Variable -Name locationResult -Value $combobox1.selectedItem -Force -Scope Script # Use this
-    $script:locationResult = $cBox2.selectedItem # or this to retrieve the user selection
-})
-  
-$form.Controls.Add($button)
-$form.Controls.Add($cBox2)
-  
-$form.ShowDialog()
-  
-$grouptag = $script:locationResult
-Write-Output $grouptag
 
 Write-Host -BackgroundColor Black -ForegroundColor Green "Start AutoPilot Verification"
 $body = $bodyMessage | ConvertTo-Json -Depth 5; $uri = 'https://prod-145.westus.logic.azure.com:443/workflows/dadfcaca1bcc4b069c998a99e82ee728/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=n0urWoGWa2OXN-4ba0U7UwfEM8i9vwTuSHx2PrSVtvU'
@@ -110,6 +35,81 @@ if ($result.Response -eq 0) {
 
 
     if (Test-Path .\OA3.xml) {
+        ###################Load Assembly for creating form & button######
+  
+        [void][System.Reflection.Assembly]::LoadWithPartialName( "System.Windows.Forms")
+        [void][System.Reflection.Assembly]::LoadWithPartialName( "Microsoft.VisualBasic")
+  
+  
+        #####Define the form size & placement
+        
+        $form = New-Object "System.Windows.Forms.Form";
+        $form.Width = 500;
+        $form.Height = 150;
+        $form.Text = "Digital Workplace Group Tag";
+        $form.StartPosition = [System.Windows.Forms.FormStartPosition]::CenterScreen;
+        $form.ControlBox = $True
+        
+        
+        ##############Define text label2
+        
+        $textLabel2 = New-Object "System.Windows.Forms.Label";
+        $textLabel2.Left = 25;
+        $textLabel2.Top = 45;
+        $textLabel2.Text = "Group tag";
+        
+        
+        ############Define text box2 for input
+        
+        $cBox2 = New-Object "System.Windows.Forms.combobox";
+        $cBox2.Left = 150;
+        $cBox2.Top = 45;
+        $cBox2.width = 200;
+        $cBox2.Text = "Choose group tag"
+        
+        
+        ###############"Add descriptions to combo box"##############
+
+        Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Lenander88/L88/main/grouptags.csv' -Outfile grouptags.csv 
+        Import-CSV ".\grouptags.csv" | ForEach-Object {
+            $cBox2.Items.Add($_.grouptags)| out-null
+            
+        }
+  
+  
+        #############define OK button
+        $button = New-Object "System.Windows.Forms.Button";
+        $button.Left = 360;
+        $button.Top = 45;
+        $button.Width = 100;
+        $button.Text = "OK";
+        $Button.Cursor = [System.Windows.Forms.Cursors]::Hand
+        #$Button.Font = New-Object System.Drawing.Font("Times New Roman",10,[System.Drawing.FontStyle]::Regular)
+        ############# This is when you have to close the form after getting values
+        $eventHandler = [System.EventHandler]{
+        $cBox2.Text;
+        $form.Close();};
+        $button.Add_Click($eventHandler) ;
+        
+        #############Add controls to all the above objects defined
+        $form.Controls.Add($button);
+        $form.Controls.Add($textLabel2);
+        $form.Controls.Add($cBox2);
+        #$ret = $form.ShowDialog();
+        
+        #################return values
+        $button.add_Click({    
+
+            $script:locationResult = $cBox2.selectedItem 
+        })
+  
+        $form.Controls.Add($button)
+        $form.Controls.Add($cBox2)
+  
+        $form.ShowDialog()
+  
+        $grouptag = $script:locationResult
+        Write-Output $grouptag
 
         [xml]$xmlhash = Get-Content -Path .\OA3.xml
         $hash=$xmlhash.Key.HardwareHash
