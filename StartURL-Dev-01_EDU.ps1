72c9787629c35b6fed38a530fbfbabe93d410193
Lenander88 <167141130+Lenander88@users.noreply.github.com>
2025-11-14 13:52:26 +0100

    more changes

--- StartURL-Dev-01_EDU.ps1
+++ StartURL-Dev-01_EDU.ps1
@@ -33,7 +33,6 @@ if (!(Test-Path $csvPath) -or ((Get-Item $csvPath).LastWriteTime -lt (Get-Date).
 }
 
 $options = Import-CSV $csvPath
-$options = Import-CSV ".\EDU.csv"
 
 # Create Form
 $form = New-Object System.Windows.Forms.Form
@@ -117,6 +116,63 @@ if ($result.Response -eq 0) {
     .\oa3tool.exe /Report /ConfigFile=.\OA3.cfg /NoKeyCheck
 
     if (Test-Path .\OA3.xml) {        
+        # Path to CSV file
+        $csvPath = ".\grouptags.csv"
+        $csvUrl = 'https://raw.githubusercontent.com/Lenander88/L88/main/grouptags_dev.csv'
+        if (!(Test-Path $csvPath) -or ((Get-Item $csvPath).LastWriteTime -lt (Get-Date).AddDays(-1))) {
+            Invoke-WebRequest -Uri $csvUrl -OutFile $csvPath
+        }
+
+        $options = Import-CSV $csvPath
+
+        # Create Form
+        $form = New-Object System.Windows.Forms.Form
+        $form.Text = "Digital Workplace Group Tag"
+        $form.Size = New-Object System.Drawing.Size(300,150)
+        $form.StartPosition = "CenterScreen"
+        $form.ControlBox = $True
+
+        $textLabel2 = New-Object "System.Windows.Forms.Label";
+        $textLabel2.Left = 25;
+        $textLabel2.Top = 45;
+        $textLabel2.Text = "Digital Workplace Group Tag";
+
+        # Create ComboBox
+        $comboBox = New-Object System.Windows.Forms.ComboBox
+        $comboBox.Location = New-Object System.Drawing.Point(50,20)
+        $comboBox.Size = New-Object System.Drawing.Size(200,20)
+        $comboBox.DropDownStyle = 'DropDownList'  # Prevent typing, only select
+
+        # Populate ComboBox with OptionName from CSV
+        foreach ($item in $options) {
+            $comboBox.Items.Add($item.OptionName)
+        }
+
+        $form.Controls.Add($comboBox)
+
+        # Create OK Button
+        $okButton = New-Object System.Windows.Forms.Button
+        $okButton.Text = "OK"
+        $okButton.Location = New-Object System.Drawing.Point(100,60)
+
+        $okButtonClickHandler = {
+            $selectedOption = $comboBox.SelectedItem
+            if ($selectedOption) {
+                # Assign corresponding Value to $grouptag (hidden from user)
+                $global:grouptag = ($options | Where-Object { $_.OptionName -eq $selectedOption }).Value
+                $form.Close()
+            } else {
+                [System.Windows.Forms.MessageBox]::Show("Please select an option.")
+            }
+        }
+
+        $okButton.Add_Click($okButtonClickHandler)
+
+        $form.Controls.Add($okButton)
+
+        # Show Form
+        $form.ShowDialog()
+        
         $form = New-Object "System.Windows.Forms.Form";
         $form.Width = 500;
         $form.Height = 150;
